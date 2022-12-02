b32d8207b4a1ed512c377e1c350299f47bfa00b4
Marco Cafasso <marcocafassog@gmail.com>
2022-12-02 17:18:37 +0100

    feat(MainController): add getBestMovies method
    method which returns the page best_movies and create a list of movies

--- SpringMovieSong/src/main/java/org/generation/italy/demo/controller/MainController.java
+++ SpringMovieSong/src/main/java/org/generation/italy/demo/controller/MainController.java
@@ -1,5 +1,9 @@
 package org.generation.italy.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.generation.italy.demo.pojo.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +19,21 @@ public class MainController {
 		model.addAttribute("name", name);
 		
 		return "home"; 
+	}
+	
+	@RequestMapping("/best_movies")
+	public String getBestMovies(Model model) {
+		String name = "markcaf";
+		model.addAttribute("name", name);
+		
+		List<Movie> movies = new ArrayList<>();
+		movies.add(new Movie(1, "The Lord Of The Rings"));
+		movies.add(new Movie(2, "Star Wars"));
+		movies.add(new Movie(5, "Back To The Future"));
+		movies.add(new Movie(9, "Shutter Island"));
+
+		model.addAttribute("movies", movies);
+
+		return "best_movies"; 
 	}
 }
